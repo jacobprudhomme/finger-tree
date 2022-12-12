a2a033363f11f6d779b59ddfcfdc3ff3b0a2b77c
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-12 22:51:11 +0100

    Made all type parameters covariant

--- src/main/scala/fingertree/FingerTree.scala
+++ src/main/scala/fingertree/FingerTree.scala
@@ -2,20 +2,20 @@ package fingertree
 
 import stainless.collection._
 
-private sealed trait Node[T]
-private final case class Node2[T](a: T, b: T) extends Node[T]
-private final case class Node3[T](a: T, b: T, c: T) extends Node[T]
+private sealed trait Node[+T]
+private final case class Node2[+T](a: T, b: T) extends Node[T]
+private final case class Node3[+T](a: T, b: T, c: T) extends Node[T]
 
-private sealed trait Digit[T]
-private final case class Digit1[T](a: T) extends Digit[T]
-private final case class Digit2[T](a: T, b: T) extends Digit[T]
-private final case class Digit3[T](a: T, b: T, c: T) extends Digit[T]
-private final case class Digit4[T](a: T, b: T, c: T, d: T) extends Digit[T]
+private sealed trait Digit[+T]
+private final case class Digit1[+T](a: T) extends Digit[T]
+private final case class Digit2[+T](a: T, b: T) extends Digit[T]
+private final case class Digit3[+T](a: T, b: T, c: T) extends Digit[T]
+private final case class Digit4[+T](a: T, b: T, c: T, d: T) extends Digit[T]
 
-sealed trait FingerTree[T]
-private final case class Empty[T]() extends FingerTree[T]
-private final case class Single[T](value: T) extends FingerTree[T]
-private final case class Deep[T](
+sealed trait FingerTree[+T]
+private case object Empty extends FingerTree[Nothing]
+private final case class Single[+T](value: T) extends FingerTree[T]
+private final case class Deep[+T](
   prefix: Digit[T],
   spine: FingerTree[Node[T]],
   suffix: Digit[T]
@@ -24,11 +24,11 @@ private final case class Deep[T](
 object FingerTree {
   def addL[T](tree: FingerTree[T], value: T): FingerTree[T] =
     tree match {
-      case Empty() => Single(value)
+      case Empty => Single(value)
       case Single(existingValue) =>
         Deep(
           Digit1(value),
-          Empty(),
+          Empty,
           Digit1(existingValue)
         )
       case Deep(Digit1(a), spine, suffix) =>
@@ -59,11 +59,11 @@ object FingerTree {
 
   def addR[T](tree: FingerTree[T], value: T): FingerTree[T] =
     tree match {
-      case Empty() => Single(value)
+      case Empty => Single(value)
       case Single(existingValue) =>
         Deep(
           Digit1(existingValue),
-          Empty(),
+          Empty,
           Digit1(value)
         )
       case Deep(prefix, spine, Digit1(a)) =>
