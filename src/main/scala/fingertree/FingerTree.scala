92e9f402ce856932c4c90722f565b1ecf529d2e2
Pierre Gimalac <pierre.gimalac@gmail.com>
2022-12-21 01:52:32 +0100

    format

--- src/main/scala/fingertree/FingerTree.scala
+++ src/main/scala/fingertree/FingerTree.scala
@@ -146,70 +146,30 @@ object FingerTree {
     tree match {
       case Empty() => Single(value)
       case Single(existingValue) =>
-        Deep(
-          Digit1(value),
-          Empty(),
-          Digit1(existingValue)
-        )
+        Deep(Digit1(value), Empty(), Digit1(existingValue))
       case Deep(Digit1(a), spine, suffix) =>
-        Deep(
-          Digit2(value, a),
-          spine,
-          suffix
-        )
+        Deep(Digit2(value, a), spine, suffix)
       case Deep(Digit2(a, b), spine, suffix) =>
-        Deep(
-          Digit3(value, a, b),
-          spine,
-          suffix
-        )
+        Deep(Digit3(value, a, b), spine, suffix)
       case Deep(Digit3(a, b, c), spine, suffix) =>
-        Deep(
-          Digit4(value, a, b, c),
-          spine,
-          suffix
-        )
+        Deep(Digit4(value, a, b, c), spine, suffix)
       case Deep(Digit4(a, b, c, d), spine, suffix) =>
-        Deep(
-          Digit2(value, a),
-          addL(spine, Node3(b, c, d)),
-          suffix
-        )
+        Deep(Digit2(value, a), addL(spine, Node3(b, c, d)), suffix)
     }
 
   def addR[T](tree: FingerTree[T], value: T): FingerTree[T] =
     tree match {
       case Empty() => Single(value)
       case Single(existingValue) =>
-        Deep(
-          Digit1(existingValue),
-          Empty(),
-          Digit1(value)
-        )
+        Deep(Digit1(existingValue), Empty(), Digit1(value))
       case Deep(prefix, spine, Digit1(a)) =>
-        Deep(
-          prefix,
-          spine,
-          Digit2(a, value)
-        )
+        Deep(prefix, spine, Digit2(a, value))
       case Deep(prefix, spine, Digit2(a, b)) =>
-        Deep(
-          prefix,
-          spine,
-          Digit3(a, b, value)
-        )
+        Deep(prefix, spine, Digit3(a, b, value))
       case Deep(prefix, spine, Digit3(a, b, c)) =>
-        Deep(
-          prefix,
-          spine,
-          Digit4(a, b, c, value)
-        )
+        Deep(prefix, spine, Digit4(a, b, c, value))
       case Deep(prefix, spine, Digit4(a, b, c, d)) =>
-        Deep(
-          prefix,
-          addR(spine, Node3(a, b, c)),
-          Digit2(d, value)
-        )
+        Deep(prefix, addR(spine, Node3(a, b, c)), Digit2(d, value))
     }
 
   def viewL[T](tree: FingerTree[T]): View[T] =
