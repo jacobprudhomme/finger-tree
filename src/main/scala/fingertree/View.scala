9ce36ab7d6476922e13479798651e9a1350b5da8
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2023-01-09 21:16:00 +0100

    Added comments for View

--- src/main/scala/fingertree/View.scala
+++ src/main/scala/fingertree/View.scala
@@ -1,11 +1,20 @@
 package fingertree
 
+/// This file defines the data structure for a view of a finger tree, a
+/// decomposition of it into a chosen element and the rest of the tree.
+/// The case classes of View[T] are found at the end of the file.
+
 sealed trait View[T]:
+
+  /// Determines if we have the view of an empty finger tree
   def isEmpty: Boolean =
     this match {
       case NilV()      => true
       case ConsV(_, _) => false
     }
 
+/// A View[T] is either a:
+/// - NilV[T](), or
+/// - ConsV[T](T, FingerTree[T]),
 final case class NilV[T]() extends View[T]
 final case class ConsV[T](head: T, rest: FingerTree[T]) extends View[T]
